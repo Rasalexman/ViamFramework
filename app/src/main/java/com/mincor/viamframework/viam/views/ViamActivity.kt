519b541879f966ac5040879aa7a38b9e598380fe
Alexander <sphc@yandex.ru>
2018-05-16 01:57:48 +0300

    activity recall

--- app/src/main/java/com/mincor/viamframework/viam/views/ViamActivity.kt
+++ app/src/main/java/com/mincor/viamframework/viam/views/ViamActivity.kt
@@ -3,6 +3,7 @@ package com.mincor.viamframework.viam.views
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mincor.viamframework.viam.base.events.EventDispatcher
+import com.mincor.viamframework.viam.base.ext.className
 
 abstract class ViamActivity : AppCompatActivity() {
 
@@ -13,7 +14,7 @@ abstract class ViamActivity : AppCompatActivity() {
          */
         try {
             (this.application as IApplication).viamContext.injectViewController(this)
-            EventDispatcher.setDispatcher("${this::class.qualifiedName}${this.hashCode()}")
+            EventDispatcher.setDispatcher("${this::class.className()}${this.hashCode()}")
         } catch (e: Exception) {
             e.printStackTrace()
             throw RuntimeException("Please create a custom ViamApplication and fill in the getViamContextInstance() method")
@@ -23,7 +24,7 @@ abstract class ViamActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         try {
-            EventDispatcher.removeDispatcher("${this::class.qualifiedName}${this.hashCode()}")
+            EventDispatcher.removeDispatcher("${this::class.className()}${this.hashCode()}")
             (this.application as IApplication).viamContext.removeViewController(this)
         } catch (e: Exception) {
             e.printStackTrace()
